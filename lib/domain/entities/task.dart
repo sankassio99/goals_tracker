0b420cbf910b76260899305f7dd76ddbcf58fc2b
sankassio99 <kciosantana@gmail.com>
2023-01-28 16:04:37 -0300

    Task should Mark as completed(RED)

--- lib/domain/entities/task.dart
+++ lib/domain/entities/task.dart
@@ -3,4 +3,8 @@ class Task {
   bool completeStatus;
 
   Task(this.title, {this.completeStatus = false});
+
+  bool get isCompleted => completeStatus;
+
+  void markAsCompleted() {}
 }
---
+++ test/task_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:goals_tracker/domain/entities/task.dart';
+
+void main() {
+  setUp(() async {});
+  group('Task should', () {
+    test('Mark as completed', () async {
+      //#region Arrange(Given)
+      String title = "Group class";
+      Task task = Task(title);
+      //#endregion
+
+      //#region Act(When)
+      task.markAsCompleted();
+      //#endregion
+
+      //#region Assert(Then)
+      expect(task.isCompleted, isTrue);
+      //#endregion
+    });
+  });
+}
