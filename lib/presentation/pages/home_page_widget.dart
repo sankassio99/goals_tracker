a4870f1abe9c5720b78ae33b2d6f8578b41b7238
sankassio99 <kciosantana@gmail.com>
2023-01-29 11:20:21 -0300

    When is loaded must be show a title and button to add new goals (RED)

---
+++ lib/presentation/pages/home_page_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePageWidget extends StatelessWidget {
+  const HomePageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ test/home_widget_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:goals_tracker/presentation/pages/home_page_widget.dart';
+
+void main() {
+  testWidgets('When is loaded must be show a title and button to add new goals',
+      (WidgetTester tester) async {
+    // arrange
+    await tester.pumpWidget(const HomePageWidget());
+
+    // act
+    await tester.pumpAndSettle();
+
+    // assert
+    var titleFinder = find.byKey(const Key("homeTitle"));
+    var buttonFinder = find.byKey(const Key("addNewGoalButton"));
+
+    expect(titleFinder, findsOneWidget);
+    expect(buttonFinder, findsOneWidget);
+  });
+
+  testWidgets(
+      'When add main goal button is clicked must be redirect to new maingoal page created',
+      (WidgetTester tester) async {
+    // arrange
+    await tester.pumpWidget(const HomePageWidget());
+
+    // act
+
+    // assert
+  });
+
+  testWidgets(
+      'When new main goal page is created the focus must be in title input',
+      (WidgetTester tester) async {
+    // arrange
+    await tester.pumpWidget(const HomePageWidget());
+
+    // act
+
+    // assert
+  });
+
+  testWidgets('When is loaded must be show a list of main goal cards',
+      (WidgetTester tester) async {
+    // arrange
+    await tester.pumpWidget(const HomePageWidget());
+
+    // act
+
+    // assert
+  });
+}
