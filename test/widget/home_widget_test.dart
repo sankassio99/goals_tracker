14a84b6b54e5d10c075c4105d2642ec62af005c1
sankassio99 <kciosantana@gmail.com>
2023-02-04 12:45:00 -0300

    When new main goal page is created the focus must be in title input (RED)

--- test/widget/home_widget_test.dart
+++ test/widget/home_widget_test.dart
@@ -42,11 +42,18 @@ void main() {
       'When new main goal page is created the focus must be in title input',
       (WidgetTester tester) async {
     // arrange
-    // await tester.pumpWidget(const HomePageWidget());
+    await tester.pumpWidget(const MyApp());
 
     // act
+    var buttonFinder = find.byKey(const Key("addNewGoalButton"));
+    await tester.tap(buttonFinder);
+    await tester.pumpAndSettle();
 
     // assert
+    var titleInputFinder = find.byKey(const Key("titleInput"));
+    TextField titleInputWidget = tester.widget(titleInputFinder);
+    var hasFocus = titleInputWidget.focusNode!.hasPrimaryFocus;
+    expect(hasFocus, isTrue);
   });
 
   testWidgets('When is loaded must be show a list of main goal cards',
