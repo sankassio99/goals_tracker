0845c18af8a721b664c59a02fe0d4d40fd438306
Kássio San <kciosantana@gmail.com>
2023-04-28 21:43:32 -0300

    Add new goal use cases should create a new goal and save on repository

--- lib/application/usecases/add_new_goal.dart
+++ lib/application/usecases/add_new_goal.dart
@@ -1,7 +1,9 @@
 import 'package:goals_tracker/domain/entities/goal.dart';
 
+import '../adapters/igoal_repository.dart';
+
 class AddNewGoal {
-  AddNewGoal();
+  AddNewGoal(IGoalRepository goalRepository);
 
   String execute() {
     print("Creating a new goal");
