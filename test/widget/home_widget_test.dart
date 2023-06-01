04149652e627ee70c5762e7d681d34aa9def1651
Kássio San <kciosantana@gmail.com>
2023-05-31 21:15:32 -0300

    When is loaded must be show a title and button to add new goals (GREEN)

--- test/widget/home_widget_test.dart
+++ test/widget/home_widget_test.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:goals_tracker/application/usecases/add_new_goal.dart';
+import 'package:goals_tracker/application/usecases/add_subgoal.dart';
+import 'package:goals_tracker/application/usecases/get_goals.dart';
+import 'package:goals_tracker/application/usecases/update_goal.dart';
+import 'package:goals_tracker/infra/goal_repository.dart';
+import 'package:goals_tracker/infra/sub_goal_repository.dart';
 import 'package:goals_tracker/main.dart';
+import 'package:goals_tracker/presentation/controllers/home_controller.dart';
+import 'package:goals_tracker/presentation/pages/home_page_widget.dart';
 import 'package:goals_tracker/presentation/pages/main_goal_page_widget.dart';
+import 'package:provider/provider.dart';
 
 void main() {
+  late AddNewGoal addNewGoal;
+  late GetGoals getGoals;
+  late UpdateGoal updateGoal;
+
+  setUp(() {
+    var goalRepository = GoalRepositoryMemory();
+    addNewGoal = AddNewGoal(goalRepository);
+    updateGoal = UpdateGoal(goalRepository);
+    getGoals = GetGoals(goalRepository);
+  });
+
   testWidgets('When is loaded must be show a title and button to add new goals',
       (WidgetTester tester) async {
     // arrange
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => HomeController(
+              addNewGoal,
+              getGoals,
+              updateGoal,
+            ),
+          ),
+        ],
+        child: Builder(builder: (_) => MyApp()),
+      ),
+    );
 
     // act
     await tester.pumpAndSettle();
