6af8229cbfb8a151241ab05c5b91b8ee303eb224
Kássio San <kciosantana@gmail.com>
2023-06-03 11:14:36 -0300

    When Header Goal Widget is loaded must display title and description (GREEN)

--- test/widget/header_goal_widget_test.dart
+++ test/widget/header_goal_widget_test.dart
@@ -37,4 +37,36 @@ void main() {
     var input = find.byKey(const Key("descInput"));
     expect(input, findsOneWidget);
   });
+
+  testWidgets(
+      'When Header Goal Widget is loaded must display title and description',
+      (WidgetTester tester) async {
+    // arrange
+    var title = "Buy a new car";
+    var description = "Lorem ipsum";
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: HeaderGoalWidget(
+              model: GoalModel(
+            "",
+            name: title,
+            description: description,
+          )),
+        ),
+      ),
+    );
+
+    // act
+
+    // assert
+    var inputTitle = find.byKey(const Key("titleInput"));
+    var inputDesc = find.byKey(const Key("descInput"));
+
+    TextField inputTitleWidget = tester.widget(inputTitle);
+    expect(inputTitleWidget.controller!.text, title);
+
+    TextField inputDescWidget = tester.widget(inputDesc);
+    expect(inputDescWidget.controller!.text, description);
+  });
 }
