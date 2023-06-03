affc102276b8e79cb253a18309af01f68cc1f130
Kássio San <kciosantana@gmail.com>
2023-06-03 12:49:23 -0300

    When Main Goal Page Widget is loaded must show goal title (RED)

--- test/widget/main_goal_widget_test.dart
+++ test/widget/main_goal_widget_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:goals_tracker/main.dart';
 import 'package:goals_tracker/presentation/components/header_goal_widget.dart';
+import 'package:goals_tracker/presentation/controllers/main_goal_controller.dart';
 import 'package:goals_tracker/presentation/models/goal_model.dart';
 import 'package:goals_tracker/presentation/pages/main_goal_page_widget.dart';
 
@@ -21,4 +24,46 @@ void main() {
     var header = find.byType(HeaderGoalWidget);
     expect(header, findsOneWidget);
   });
+
+  testWidgets('When Main Goal Page Widget is loaded must show goal title',
+      (WidgetTester tester) async {
+    var goalId = "1";
+    var title = "Buy new car";
+
+    // arrange
+    await tester.pumpWidget(GetMaterialApp(
+      initialRoute: '/mainGoalDetails/:$goalId',
+      getPages: [
+        GetPage(
+          name: '/mainGoalDetails/:goalId',
+          page: () => MainGoalPageWidget(
+            model: GoalModel("id"),
+          ),
+          binding: MainGoalBinding(),
+        ),
+      ],
+    ));
+    // act
+    var inputTitle = find.byKey(const Key("titleInput"));
+
+    // assert
+    TextField inputTitleWidget = tester.widget(inputTitle);
+    expect(inputTitleWidget.controller!.text, title);
+  });
+}
+
+class MainGoalBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => MainGoalController());
+  }
+}
+
+class MainGoalController extends GetxController {
+  @override
+  void onInit() {
+    print("On initi called");
+    print(Get.parameters['goalId']);
+    super.onInit();
+  }
 }
