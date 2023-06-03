35f74a466872364a1578faa9d4f744f180ef501f
Kássio San <kciosantana@gmail.com>
2023-06-03 16:01:42 -0300

    infra configs

--- lib/infra/dependecy_binds.dart
+++ lib/infra/dependecy_binds.dart
@@ -2,14 +2,19 @@ import 'package:get/get.dart';
 import 'package:goals_tracker/application/adapters/igoal_repository.dart';
 import 'package:goals_tracker/application/usecases/add_new_goal.dart';
 import 'package:goals_tracker/application/usecases/get_goal_details.dart';
+import 'package:goals_tracker/infra/goal_repository.dart';
 import 'package:goals_tracker/presentation/controllers/home_controller.dart';
 import 'package:goals_tracker/presentation/controllers/main_goal_controller.dart';
 
 class MainGoalBinding extends Bindings {
   @override
   void dependencies() {
-    var goalRepository = Get.find<IGoalRepository>();
+    var goalRepository = Get.isRegistered<IGoalRepository>()
+        ? Get.find<IGoalRepository>()
+        : GoalRepositoryMemory();
+
     var getGoalDetails = GetGoalDetails(goalRepository);
+
     Get.lazyPut(() => MainGoalController(getGoalDetails));
   }
 }
@@ -17,7 +22,10 @@ class MainGoalBinding extends Bindings {
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    var goalRepository = Get.find<IGoalRepository>();
+    var goalRepository = Get.isRegistered<IGoalRepository>()
+        ? Get.find<IGoalRepository>()
+        : GoalRepositoryMemory();
+
     var addNewGoal = AddNewGoal(goalRepository);
     Get.lazyPut(() => HomeController(addNewGoal));
   }
