27ae7a179d0fefb1ec78341d958ea4235d7283a3
Kássio San <kciosantana@gmail.com>
2023-06-03 09:26:06 -0300

    When a goal card is tapped must be redirect to main goal page (RED)

--- test/widget/home_widget_test.dart
+++ test/widget/home_widget_test.dart
@@ -6,9 +6,7 @@ import 'package:goals_tracker/application/usecases/update_goal.dart';
 import 'package:goals_tracker/infra/goal_repository.dart';
 import 'package:goals_tracker/main.dart';
 import 'package:goals_tracker/presentation/components/goal_card_widget.dart';
-import 'package:goals_tracker/presentation/controllers/home_controller.dart';
 import 'package:goals_tracker/presentation/pages/main_goal_page_widget.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   late AddNewGoal addNewGoal;
@@ -53,30 +51,17 @@ void main() {
     expect(goalCard, findsOneWidget);
   });
 
-  testWidgets(
-      'When add main goal button is clicked must be redirect to new maingoal page created',
+  testWidgets('When a goal card is tapped must be redirect to main goal page',
       (WidgetTester tester) async {
     // arrange
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (_) => HomeController(
-              addNewGoal,
-              getGoals,
-              updateGoal,
-            ),
-          ),
-        ],
-        child: Builder(builder: (_) => const MyApp()),
-      ),
-    );
+    await tester.pumpWidget(const MyApp());
+    var buttonFinder = find.byKey(const Key("addNewGoalButton"));
+    await tester.tap(buttonFinder);
+    await tester.pumpAndSettle();
 
     // act
-    var buttonFinder = find.byKey(const Key("addNewGoalButton"));
-
-    await tester.tap(buttonFinder);
-
+    var goalCard = find.byType(GoalCardWidget);
+    await tester.tap(goalCard);
     await tester.pumpAndSettle();
 
     // assert
