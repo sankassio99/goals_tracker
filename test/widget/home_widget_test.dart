6a282b0b5f7ee917402190215ddcc82881cf898b
Kássio San <kciosantana@gmail.com>
2023-06-03 08:53:04 -0300

    When is add new goal must update goal list view with goal card (RED)

--- test/widget/home_widget_test.dart
+++ test/widget/home_widget_test.dart
@@ -5,6 +5,7 @@ import 'package:goals_tracker/application/usecases/get_goals.dart';
 import 'package:goals_tracker/application/usecases/update_goal.dart';
 import 'package:goals_tracker/infra/goal_repository.dart';
 import 'package:goals_tracker/main.dart';
+import 'package:goals_tracker/presentation/components/goal_card_widget.dart';
 import 'package:goals_tracker/presentation/controllers/home_controller.dart';
 import 'package:goals_tracker/presentation/pages/main_goal_page_widget.dart';
 import 'package:provider/provider.dart';
@@ -35,6 +36,21 @@ void main() {
 
     expect(titleFinder, findsOneWidget);
     expect(buttonFinder, findsOneWidget);
+  });
+
+  testWidgets('When is add new goal must update goal list view with goal card',
+      (WidgetTester tester) async {
+    // arrange
+    await tester.pumpWidget(const MyApp());
+    var buttonFinder = find.byKey(const Key("addNewGoalButton"));
+
+    // act
+    await tester.tap(buttonFinder);
+    await tester.pumpAndSettle();
+
+    // assert
+    var goalCard = find.byType(GoalCardWidget);
+    expect(goalCard, findsOneWidget);
   });
 
   testWidgets(
