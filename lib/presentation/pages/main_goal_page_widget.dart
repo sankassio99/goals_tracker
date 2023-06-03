b7a48ed54087900ba60ee15d44bf5fc30248780f
Kássio San <kciosantana@gmail.com>
2023-06-03 19:44:10 -0300

    floatingActionButton

--- lib/presentation/pages/main_goal_page_widget.dart
+++ lib/presentation/pages/main_goal_page_widget.dart
@@ -15,66 +15,50 @@ class MainGoalPageWidget extends StatelessWidget {
     controller.getGoal();
     return GetBuilder<MainGoalController>(builder: (controller) {
       return Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        appBar: const MyAppBar(),
-        body: SafeArea(
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-            ),
-            child: Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 213,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                      ),
-                      child: HeaderGoalWidget(
-                        model: controller.goalModel.value,
+          backgroundColor: Theme.of(context).primaryColor,
+          appBar: const MyAppBar(),
+          body: SafeArea(
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 213,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        child: HeaderGoalWidget(
+                          model: controller.goalModel.value,
+                        ),
                       ),
                     ),
-                  ),
-                  const Divider(
-                    height: 10,
-                    thickness: 1,
-                    indent: 50,
-                    endIndent: 50,
-                    color: Colors.black12,
-                  ),
-                  // Consumer<MainGoalController>(
-                  //     builder: (context, controller, child) {
-                  //   return ListView(
-                  //     key: const Key("goalCardsListView"),
-                  //     padding: EdgeInsets.zero,
-                  //     shrinkWrap: true,
-                  //     scrollDirection: Axis.vertical,
-                  //     children: controller.currentGoal.subGoals
-                  //         .map((goal) => SubGoalCardWidget(model: goal))
-                  //         .toList(),
-                  //   );
-                  // }),
-                ],
+                    const Divider(
+                      height: 10,
+                      thickness: 1,
+                      indent: 50,
+                      endIndent: 50,
+                      color: Colors.black12,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        persistentFooterButtons: [
-          BottomButton(
-            label: "Add sub goal",
-            action: () {},
-          )
-        ],
-      );
+          floatingActionButton: FloatingActionButton(
+              child: const Icon(Icons.add), onPressed: () {}));
     });
   }
 }
