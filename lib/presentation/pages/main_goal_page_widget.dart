99cee355078e478123a1bdd0dcd54189a4bbb8c0
Kássio San <kciosantana@gmail.com>
2023-06-03 20:38:42 -0300

    layout

--- lib/presentation/pages/main_goal_page_widget.dart
+++ lib/presentation/pages/main_goal_page_widget.dart
@@ -78,7 +78,7 @@ class Tasks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height - 500,
+      height: MediaQuery.of(context).size.height - 400,
       child: SingleChildScrollView(
         child: Obx(
           () => Column(
