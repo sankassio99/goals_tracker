6c84bdc4c4ab81f4c254014978603f7c5754be15
Kássio San <kciosantana@gmail.com>
2023-06-04 17:36:35 -0300

    fix tests

--- test/widget/task_item_widget_test.dart
+++ test/widget/task_item_widget_test.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get/get_rx/get_rx.dart';
+import 'package:get/get.dart';
+import 'package:goals_tracker/application/usecases/get_goal_details.dart';
+import 'package:goals_tracker/application/usecases/update_goal.dart';
+import 'package:goals_tracker/presentation/controllers/main_goal_controller.dart';
 import 'package:goals_tracker/presentation/models/goal_model.dart';
 import 'package:goals_tracker/presentation/pages/main_goal_page_widget.dart';
 
+import '../add_new_goal_test.mocks.dart';
+
 void main() {
+  setUp(() {
+    var goalRepositoryMock = MockIGoalRepository();
+    var getGoalDetails = GetGoalDetails(goalRepositoryMock);
+    var updateGoal = UpdateGoal(goalRepositoryMock);
+
+    Get.lazyPut(() => MainGoalController(
+          getGoalDetails,
+          updateGoal,
+        ));
+  });
+
   testWidgets('Should load task list', (tester) async {
     //#region Arrange(Given)
 
