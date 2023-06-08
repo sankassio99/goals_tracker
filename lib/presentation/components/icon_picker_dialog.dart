f3f101d1fee909032a95fefcc280208577df90c0
Kássio San <kciosantana@gmail.com>
2023-06-08 18:52:24 -0300

    Icon Picker Dialog Widget should display all 12 icons (REFACTOR)

--- lib/presentation/components/icon_picker_dialog.dart
+++ lib/presentation/components/icon_picker_dialog.dart
@@ -5,6 +5,21 @@ class IconPickerDialog extends StatelessWidget {
     super.key,
   });
 
+  final List<IconData> iconsData = const [
+    Icons.access_alarm,
+    Icons.account_tree_sharp,
+    Icons.safety_check,
+    Icons.laptop,
+    Icons.girl,
+    Icons.boy,
+    Icons.car_rental,
+    Icons.money,
+    Icons.map,
+    Icons.wind_power,
+    Icons.account_tree_sharp,
+    Icons.gamepad,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -22,80 +37,16 @@ class IconPickerDialog extends StatelessWidget {
                   width: 200,
                   child: GridView.count(
                     crossAxisCount: 4,
-                    children: const [
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                      Icon(
-                        key: Key("iconItemChoice"),
-                        Icons.wind_power,
-                        color: Colors.black87,
-                        size: 24,
-                      ),
-                    ],
+                    children: iconsData
+                        .map(
+                          (iconData) => Icon(
+                            key: const Key("iconItemChoice"),
+                            iconData,
+                            color: Colors.black87,
+                            size: 24,
+                          ),
+                        )
+                        .toList(),
                   ),
                 ),
                 TextButton(
