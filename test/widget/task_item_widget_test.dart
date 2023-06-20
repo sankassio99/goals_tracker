c3f2a648322e5a27d3079ee8fdad3b8fb41d6ea4
Kássio San <kciosantana@gmail.com>
2023-06-19 21:52:51 -0300

    When click in trash must delete task (RED)

--- test/widget/task_item_widget_test.dart
+++ test/widget/task_item_widget_test.dart
@@ -3,9 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:goals_tracker/application/usecases/get_goal_details.dart';
 import 'package:goals_tracker/application/usecases/update_goal.dart';
+import 'package:goals_tracker/presentation/components/tasks_widget.dart';
 import 'package:goals_tracker/presentation/controllers/main_goal_controller.dart';
 import 'package:goals_tracker/presentation/models/goal_model.dart';
-import 'package:goals_tracker/presentation/pages/main_goal_page_widget.dart';
 
 import '../add_new_goal_test.mocks.dart';
 
@@ -42,6 +42,35 @@ void main() {
     //#region Assert(Then)
     var tasksWidget = find.byType(CheckboxListTile);
     expect(tasksWidget, findsNWidgets(2));
+    //#endregion
+  });
+
+  testWidgets('When click in trash must delete task', (tester) async {
+    //#region Arrange(Given)
+
+    RxList<TaskModel> tasks = [TaskModel("title1"), TaskModel("title2")].obs;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: TasksWidget(tasks: tasks),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    //#endregion
+
+    //#region Act(When)
+    var taskItem = find.byKey(const Key("taskItemIcon"));
+    await tester.tap(taskItem.first);
+    await tester.pumpAndSettle();
+    //#endregion
+
+    //#region Assert(Then)
+    var tasksWidget = find.byType(CheckboxListTile);
+    expect(tasksWidget, findsOneWidget);
     //#endregion
   });
 }
