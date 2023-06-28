9fd8c00450a02ff2f04ec6bd05f27b9e299e6d54
Kássio San <kciosantana@gmail.com>
2023-06-27 21:48:29 -0300

    Add calendar

--- lib/presentation/pages/main_goal_page_widget.dart
+++ lib/presentation/pages/main_goal_page_widget.dart
@@ -7,6 +7,7 @@ import 'package:goals_tracker/presentation/components/tasks_widget.dart';
 import 'package:goals_tracker/presentation/controllers/main_goal_controller.dart';
 import 'package:goals_tracker/presentation/models/goal_model.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class MainGoalPageWidget extends StatelessWidget {
   final controller = Get.find<MainGoalController>();
@@ -108,9 +109,65 @@ class GoalTabBar extends StatelessWidget {
         body: TabBarView(
           children: [
             TasksWidget(tasks: model.tasks),
-            Icon(PhosphorIcons.bold.calendar),
+            GoalCalendarWidget(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class GoalCalendarWidget extends StatelessWidget {
+  final CalendarFormat _calendarFormat = CalendarFormat.month;
+  final DateTime _focusedDay = DateTime.now();
+
+  GoalCalendarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final kToday = DateTime.now();
+    final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
+    final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 24.0, 8.0, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Calendar",
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Container(
+            height: 400,
+            width: 500,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black12,
+                width: 2.0,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: TableCalendar(
+              focusedDay: _focusedDay,
+              firstDay: kFirstDay,
+              lastDay: kLastDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                return isSameDay(
+                    day, DateTime(kToday.year, kToday.month + 3, kToday.day));
+              },
+              headerStyle: const HeaderStyle(
+                titleCentered: true,
+                formatButtonVisible: false,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
