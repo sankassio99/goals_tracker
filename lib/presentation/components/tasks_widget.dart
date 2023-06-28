e90f0ccc9bdea4374378396f91b272d57a10c1e0
Kássio San <kciosantana@gmail.com>
2023-06-27 21:51:54 -0300

    My tasks title

--- lib/presentation/components/tasks_widget.dart
+++ lib/presentation/components/tasks_widget.dart
@@ -15,60 +15,76 @@ class TasksWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      width: double.infinity,
-      child: Obx(
-        () => Column(
-          children: tasks
-              .asMap()
-              .map(
-                (int index, TaskModel task) => MapEntry(
-                  index,
-                  Dismissible(
-                    key: Key(index.toString()),
-                    onDismissed: (_) {},
-                    child: CheckboxListTile(
-                      title: Focus(
-                        onFocusChange: (value) {
-                          if (!value) controller.updateGoal();
-                        },
-                        child: TextFormField(
-                            key: const Key("inputTask"),
-                            controller: task.name,
-                            decoration: const InputDecoration(
-                              hintText: 'Tap to edit',
-                              border: InputBorder.none,
-                            )),
-                      ),
-                      value: task.checked.value,
-                      onChanged: (value) {
-                        task.checked.value = value ?? false;
-                        controller.onTaskCheck();
-                      },
-                      secondary: InkWell(
-                        onTap: () {
-                          controller.onDeleteTask(index);
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: const Text('Task deleted!'),
-                            backgroundColor:
-                                Theme.of(context).colorScheme.background,
-                          ));
-                        },
-                        child: Icon(
-                          key: const Key("taskItemIcon"),
-                          PhosphorIcons.regular.trash,
-                          color: Theme.of(context).colorScheme.error,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 24.0, 8.0, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "My tasks",
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Obx(
+              () => Column(
+                children: tasks
+                    .asMap()
+                    .map(
+                      (int index, TaskModel task) => MapEntry(
+                        index,
+                        Dismissible(
+                          key: Key(index.toString()),
+                          onDismissed: (_) {},
+                          child: CheckboxListTile(
+                            title: Focus(
+                              onFocusChange: (value) {
+                                if (!value) controller.updateGoal();
+                              },
+                              child: TextFormField(
+                                  key: const Key("inputTask"),
+                                  controller: task.name,
+                                  decoration: const InputDecoration(
+                                    hintText: 'Tap to edit',
+                                    border: InputBorder.none,
+                                  )),
+                            ),
+                            value: task.checked.value,
+                            onChanged: (value) {
+                              task.checked.value = value ?? false;
+                              controller.onTaskCheck();
+                            },
+                            secondary: InkWell(
+                              onTap: () {
+                                controller.onDeleteTask(index);
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: const Text('Task deleted!'),
+                                  backgroundColor:
+                                      Theme.of(context).colorScheme.background,
+                                ));
+                              },
+                              child: Icon(
+                                key: const Key("taskItemIcon"),
+                                PhosphorIcons.regular.trash,
+                                color: Theme.of(context).colorScheme.error,
+                              ),
+                            ),
+                            controlAffinity: ListTileControlAffinity.leading,
+                          ),
                         ),
                       ),
-                      controlAffinity: ListTileControlAffinity.leading,
-                    ),
-                  ),
-                ),
-              )
-              .values
-              .toList(),
-        ),
+                    )
+                    .values
+                    .toList(),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
